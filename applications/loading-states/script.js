5a7af4e7c45b1b6926721ad083781ca2def10063
Sheikh Salman <sksalmanmiah@gmail.com>
2024-03-21 14:16:41 +0600

    loading states

--- applications/loading-states/script.js
+++ applications/loading-states/script.js
@@ -10,3 +10,22 @@ import {
   form,
   fetchData,
 } from './utilities';
+
+const loading$ = fromEvent(form, 'submit').pipe(
+  exhaustMap(() => {
+
+    const data$ = fetchData();
+
+    const showLoading$ = of(true).pipe(
+      delay(+showLoadingAfterField.value),
+      tap(() => showLoading(true)),
+    );
+
+    const timeToHideLoading$ = timer(+showLoadingAfterField.value).pipe()
+
+    const shouldShowLoading$ = concat(showLoading$, timeToHideLoading$,data$.pipe(tap(()=>showLoading(false))));
+
+    return race(shouldShowLoading$, data$);
+  }),
+);
+loading$.subscribe();
